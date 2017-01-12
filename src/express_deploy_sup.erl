2a065e794ef3b8872d759b1be844c1e07b8f61af
fxmy <fxmywc@gmail.com>
2017-01-12 13:24:07 +0800

    syn dep & init

--- src/express_deploy_sup.erl
+++ src/express_deploy_sup.erl
@@ -28,7 +28,8 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, [children()]} }.
+  syn:init(),
+  {ok, { {one_for_all, 0, 1}, [children()]} }.
 
 %%====================================================================
 %% Internal functions
